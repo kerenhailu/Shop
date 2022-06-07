87867d1c652a62db8671d1af9c09e6fa0267966f
kerenhailu <keren4509@gmail.com>
2022-06-07 13:07:50 +0300

    server

--- server/server.js
+++ server/server.js
@@ -1,32 +1,24 @@
 require('dotenv').config();
-require('./DB');
 const express = require('express');
-const app = express();
-const port = process.env.PORT;
+require('./DB');
 const cors = require('cors');
-const path=require('path');
-
+const app = express();
 app.use(cors());
 app.use(express.json());
-app.get('/',(req,res)=>res.send('workkk!'));
-// const passport = require('passport');
-// const passportMiddleware = require('./config/passport');
-
-// passportMiddleware(passport);
-
+const port = process.env.PORT;
+const path=require('path');
 const clothingRouter=require('./Routes/clothing-router');
 const usersRouter = require('./Routes/users-router');
 const cartRouter = require('./Routes/cart-router');
-
-// app.use('/news', passport.authenticate("jwt",{session:false}),newsRouter)
-// app.use(passport.initialize());
-
- app.use('/users', usersRouter);
+app.get('/',(req,res)=>res.send('workkk!'));
+// const passport = require('passport');
+// const passportMiddleware = require('./config/passport');
+// passportMiddleware(passport);
+app.use('/users', usersRouter);
  app.use('/clothing', clothingRouter);
  app.use('/cart', cartRouter);
-
-
-
+// app.use('/news', passport.authenticate("jwt",{session:false}),newsRouter)
+// app.use(passport.initialize());
 app.listen(process.env.PORT);
 
 if(process.env.NODE_ENV === 'production'){
