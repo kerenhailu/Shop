68e6aeb6e777b28d486d736bdba03bac6f9ef18f
kerenhailu <keren4509@gmail.com>
2022-06-07 15:14:26 +0300

    router

--- server/server.js
+++ server/server.js
@@ -3,21 +3,20 @@ const express = require('express');
 require('./DB');
 const cors = require('cors');
 const path=require('path');
-// const clothingRouter=require('./Routes/clothing-router');
+const clothingRouter=require('./Routes/clothing-router');
 // const usersRouter = require('./Routes/users-router');
-// const cartRouter = require('./Routes/cart-router');
+const cartRouter = require('./Routes/cart-router');
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = process.env.PORT;
 
-// app.get('/',(req,res)=>res.send('workkk!'));
 // const passport = require('passport');
 // const passportMiddleware = require('./config/passport');
 // passportMiddleware(passport);
 // app.use('/users', usersRouter);
-//  app.use('/clothing', clothingRouter);
-//  app.use('/cart', cartRouter);
+ app.use('/clothing', clothingRouter);
+ app.use('/cart', cartRouter);
 // app.use('/news', passport.authenticate("jwt",{session:false}),newsRouter)
 // app.use(passport.initialize());
 app.listen(process.env.PORT);
